445dc34f9a5f0d94f24fc76a923f4ad67002c203
Chris Birster <christopher.birster@gmail.com>
2026-08-29 18:00:49 -0400

    Build deez.run as a static Solid SPA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import solid from "@solidjs/vite-plugin";
+import stylex from "@stylexjs/unplugin/vite";
 
 export default defineConfig({
-  plugins: [
-    solid({
-      start: {
-        middleware: "./src/middleware.ts",
-      },
-      ssr: true,
-    }),
-  ],
+  plugins: [stylex(), solid()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      "/api": "http://127.0.0.1:5882",
+    },
+  },
 });
